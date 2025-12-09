9482d130fe0e8ee98ce0ac4efea56b71b1b305b5
Tom <tanarozova8030@gmail.com>
2025-12-10 01:57:24 +0300

    123

--- src/Living_room.java
+++ src/Living_room.java
@@ -6,17 +6,48 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+
 
 class Living_room extends JFrame  {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Living_room game = new Living_room();
     }
 
-    character fon = new character("src/image/fon1.png", 0, 0);
+    public String [] sometext = new String[]{"1","2","3","4","5"};
+   // FileReader fileReader = new FileReader("test.txt");
+
+    public int minuteCounter=0;
+
+    ActionListener al = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try (FileWriter fileWriter = new FileWriter("test.txt");){//чтобы не закрывать поток
+                fileWriter.write(sometext[minuteCounter]);
+                fileWriter.flush();  // ← ПРИНУДИТЕЛЬНАЯ запись!
+
+                System.out.println("УСПЕХ: Записано");
+            } catch (IOException a) {
+                throw new RuntimeException(a);
+            }
+            if (minuteCounter<4)
+            {
+                minuteCounter++;
+            }
+            System.out.println(1);
+            if (minuteCounter == 4) {
+                minuteCounter = 0;
+                System.out.println("Начинаем новый цикл");
+            }
+
+        }
+    };
+
+    Timer my_timer = new Timer(1000,al); //таймер через 10 секунд совершает действия в actionlistener
+
+    character fon = new character("src/image/Living_room.png", 0, 0);
     bear bear = new bear("src/image/bear.png",
             "src/image/void.png",
             "src/image/void.png",
@@ -57,14 +88,14 @@ class Living_room extends JFrame  {
 
     BufferedImage  bi;
 
-    Living_room()
-    {
+    Living_room() throws IOException {
 
 
         setSize(1920,1080);
         setVisible(true);
         addKeyListener(KL);
         bi = new BufferedImage(getWidth(), getHeight(), 2);
+        my_timer.start();
     }
 
 
@@ -82,5 +113,8 @@ class Living_room extends JFrame  {
 
         g.drawImage(bi,0,0,this);
 
+
     }
+
+
 }
