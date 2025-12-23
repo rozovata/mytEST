afc56a442ffff8dea5fdee24880be95481569df4
Tom <tanarozova8030@gmail.com>
2025-12-23 22:25:36 +0300

    Revert "sleep"
    This reverts commit 66970286

--- src/Living_room.java
+++ src/Living_room.java
@@ -14,8 +14,32 @@ class Living_room extends JFrame {
 
     public static void main(String[] args) throws IOException {
         //System.out.println("  read my text file:  "+readFile.next());
-        number = readFile.next().charAt(0)-48;
-        System.out.println(number);
+        // Открываем НОВЫЕ Scanner для чтения
+        try (Scanner s1 = new Scanner(new File("test.txt"));
+             Scanner s2 = new Scanner(new File("test2.txt"))) {
+
+            // Читаем test.txt
+            if (s1.hasNext()) {
+                String text = s1.next();
+                number1 = text.charAt(0) - '0';
+            } else {
+                number1 = 0;
+            }
+
+            // Читаем test2.txt
+            if (s2.hasNext()) {
+                String text = s2.next();
+                number2 = text.charAt(0) - '0';
+            } else {
+                number2 = 0;
+            }
+        }
+
+        System.out.println("Прочитано: number1=" + number1 + ", number2=" + number2);
+
+        //number1 = readFile.next().charAt(0)-48;
+        //number2 = readFile2.next().charAt(0)-48;
+        System.out.println(number2);
 
         Living_room game = new Living_room();
     }
@@ -30,10 +54,20 @@ class Living_room extends JFrame {
             throw new RuntimeException(e);
         }
     }
+    static FileReader fileReader2;
 
-    static Scanner readFile = new Scanner(fileReader);
-    static int number;
-    public int minuteCounter=number;
+    static {
+        try {
+            fileReader2 = new FileReader("test2.txt");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    static int number1;
+    static int number2;
+    public int minuteCounter1=number1;
+    public int minuteCounter2=number2;
+
     Sleep sleep = new Sleep("src/image/void.png",0,0);
     Shower shower = new Shower("src/image/void.png",0,0);
     Food food = new Food("src/image/void.png",0,0);
@@ -42,24 +76,24 @@ class Living_room extends JFrame {
         @Override
         public void actionPerformed(ActionEvent ee) {
             try (FileWriter fileWriter = new FileWriter("test.txt");){//чтобы не закрывать поток
-                fileWriter.write(sometext[minuteCounter]);
+                fileWriter.write(sometext[minuteCounter1]);
                 fileWriter.flush();  // ← ПРИНУДИТЕЛЬНАЯ запись!
 
                 System.out.println("УСПЕХ: Записано");
             } catch (IOException a) {
                 throw new RuntimeException(a);
             }
-            if (minuteCounter<5)
+            if (minuteCounter1<5)
             {
-                minuteCounter++;
+                minuteCounter1++;
             }
-            if (minuteCounter == 5) {
-                minuteCounter = 0;
+            if (minuteCounter1 == 5) {
+                minuteCounter1 = 0;
                 System.out.println("Начинаем новый цикл");
             }
             // Смена картинок sleep
             try {
-                switch(minuteCounter) {
+                switch(minuteCounter1) {
                     case 0:
                         sleep._image = ImageIO.read(new File("src/image/sleep1.png"));
                         break;
@@ -79,8 +113,26 @@ class Living_room extends JFrame {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
+
+            try (FileWriter fileWriter = new FileWriter("test2.txt");){//чтобы не закрывать поток
+                fileWriter.write(sometext[minuteCounter2]);
+                fileWriter.flush();  // ← ПРИНУДИТЕЛЬНАЯ запись!
+
+                System.out.println("УСПЕХ: Записано");
+            } catch (IOException a) {
+                throw new RuntimeException(a);
+            }
+            if (minuteCounter2<5)
+            {
+                minuteCounter2++;
+            }
+            if (minuteCounter2 == 5) {
+                minuteCounter2 = 0;
+                System.out.println("Начинаем новый цикл");
+            }
             try {
-                switch(minuteCounter) {
+                switch(minuteCounter2) {
                     case 0:
                         shower._image = ImageIO.read(new File("src/image/shower1.png"));
                         break;
@@ -104,8 +156,11 @@ class Living_room extends JFrame {
 
             repaint(); // <-- ВАЖНО! Перерисовать окно после смены картинки
 
+
+
         }
     };
+
 
     Timer my_timer = new Timer(1000,al); //таймер через 10 секунд совершает действия в actionlistener
 
