512b2228ae20f99280560411f48477104940b4dd
Tom <tanarozova8030@gmail.com>
2026-01-14 00:36:07 +0300

    Liv1

--- src/Bad_room.java
+++ src/Bad_room.java
@@ -1,4 +1,4 @@
-/*import javax.imageio.ImageIO;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -9,17 +9,58 @@ import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.Scanner;
 
-class Bad_room extends JFrame  {
+class Bad_room extends JFrame {
 
 
     public static void main(String[] args) throws IOException {
-        Bad_room game = new Bad_room();
+
+        Bad_room game1 = new Bad_room();
     }
+    // Инициализируем
+    String[] sleepImages = {
+            "src/image/sleep1.png",
+            "src/image/sleep2.png",
+            "src/image/sleep3.png",
+            "src/image/sleep4.png",
+            "src/image/sleep5.png"
+    };
+
+    String[] showerImages = {
+            "src/image/shower1.png",
+            "src/image/shower2.png",
+            "src/image/shower3.png",
+            "src/image/shower4.png",
+            "src/image/shower5.png"
+    };
+
+    Needs sleepNeeds = new Needs("test.txt", sleepImages);
+    Needs showerNeeds = new Needs("test2.txt", showerImages);
+    Sleep sleep = new Sleep(sleepNeeds.getCurrentImagePath(),0,0);
+    Shower shower = new Shower(showerNeeds.getCurrentImagePath(),0,0);
+    Food food = new Food("src/image/void.png",0,0);
+
+    ActionListener al = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent ee) {
+            try {
+                // Обновляем картинки в объектах
+                sleep._image = sleepNeeds.image;
+                shower._image = showerNeeds.image;
+
+                sleepNeeds.next();
+                showerNeeds.next();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            repaint();
+        }
+    };
 
 
+    Timer my_timer = new Timer(1000,al); //таймер через 10 секунд совершает действия в actionlistener
 
-
-    character fon = new character("src/image/Living_room.png", 0, 0);
+    character fon = new character("src/image/Kitchen.png", 0, 0);
     bear bear = new bear("src/image/bear.png",
             "src/image/void.png",
             "src/image/void.png",
@@ -56,8 +97,6 @@ class Bad_room extends JFrame  {
         }
     } ;
     Bad_room() throws IOException {
-
-
         setSize(1920,1080);
         setVisible(true);
         addKeyListener(KL);
@@ -88,4 +127,4 @@ class Bad_room extends JFrame  {
     }
 
 
-}*/
+}
